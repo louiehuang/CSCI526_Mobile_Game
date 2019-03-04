4e4f84a1c61f2f483bcfaaaa68479cd48fda7fd6
louiehuang <louiehuang21@gmail.com>
2019-03-04 14:58:52 -0800

    init template

---
+++ Assets/Scripts/TurretBlueprint.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class TurretBlueprint {
+
+    public GameObject prefab;
+    public int cost;
+
+    public GameObject upgradedPrefab;
+    public int upgradeCost;
+
+    public int GetSellAmount() {
+        return cost / 2;
+    }
+
+}
