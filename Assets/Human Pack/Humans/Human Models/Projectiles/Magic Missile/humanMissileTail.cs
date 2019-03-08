226537eab8a190449d6a0a7ac9994397af5b6822
Guo <guoc1995@gmail.com>
2019-03-07 17:09:58 -0800

    enemy change to monster

---
+++ Assets/Human Pack/Humans/Human Models/Projectiles/Magic Missile/humanMissileTail.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class humanMissileTail : MonoBehaviour
+{
+
+	public TrailRenderer tail;
+	public float scrollSpeed = 0.5F;
+	void Update()
+	{
+		float offset = Time.time * scrollSpeed;
+		tail.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+	}
+}
---
+++ Assets/Human Pack/Scripts/DestroyByTime.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour {
+
+	public float lifetime;
+
+	void Update () {
+
+		lifetime -= Time.deltaTime;
+
+		if (lifetime <= 0)
+			Destroy(gameObject);	
+	}
+}
